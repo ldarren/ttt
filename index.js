803daab799716e9a7272944fd4b9d3d306041c93
Darren Liew <ldarren@gmail.com>
2023-03-20 01:10:52 +0800

    flatten array

--- index.js
+++ index.js
@@ -1,123 +1,117 @@
+const ttt = document.querySelector('.tic-tac-toe')
+const cells = [...ttt.querySelectorAll('td')]
+
 // Define the players
-const player1 = 'X';
-const player2 = 'O';
+const players = ['X', '0']
+
+// Define the current player
+let currentPlayer = 0
+
 let clicks = 0
 
-function getCell(row, col){
-	const cell = document.getElementById(`c-${row}-${col}`)
+const paths = [
+	[0, 1, 2], // 0
+	[3, 4, 5], // 1
+	[6, 7, 8], // 2
+	[0, 3, 6], // 3
+	[1, 4, 7], // 4
+	[2, 5, 8], // 5
+	[0, 4, 8], // 6
+	[2, 4, 6], // 7
+]
+
+const rpaths = {
+	0: [paths[0], paths[3], paths[6]],
+	1: [paths[0], paths[4]],
+	2: [paths[0], paths[5], paths[7]],
+	3: [paths[1], paths[3]],
+	4: [paths[1], paths[4], paths[6], paths[7]],
+	5: [paths[1], paths[5]],
+	6: [paths[2], paths[3], paths[7]],
+	7: [paths[2], paths[4]],
+	8: [paths[2], paths[5], paths[6]],
+}
+
+function getCell(idx){
+	const cell = cells[idx]
 	return cell.textContent || ''
 }
 
-function setCell(row, col, mark){
-	const cell = document.getElementById(`c-${row}-${col}`)
+function setCell(idx, mark){
+	const cell = cells[idx]
 	cell.textContent = mark
 }
 
-// Define the current player
-let currentPlayer = player1;
-
 // Function to check for winning combinations
-function checkForWin() {
-  let player
-  // Check rows for winning combination
-  for (let row = 0; row < 3; row++) {
-  	player = getCell(row,0)
-    if (player !== '' && player === getCell(row,1) && player === getCell(row,2)) {
-      return player
-    }
-  }
-
-  // Check columns for winning combination
-  for (let col = 0; col < 3; col++) {
-  	player = getCell(0,col)
-    if (player !== '' && player === getCell(1,col) && player === getCell(2,col)) {
-      return player
-    }
-  }
-
-  // Check diagonals for winning combination
-  player = getCell(0,0)
-  if (player !== '' && player === getCell(1,1) && player === getCell(2,2)) {
-    return player
-  }
-  player = getCell(0,2)
-  if (player !== '' && getCell(0,2) && player === getCell(1,1) && player === getCell(2,0)) {
-    return player
-  }
-
-  return null
+function checkForWin(cell) {
+	const idx = cells.indexOf(cell)
+	const checks = rpaths[idx]
+	const mark = getCell(idx)
+	if (checks.some(check => check.every(c => cells[c].textContent === mark))) return mark
 }
 
 // Function to switch to the other player's turn
 function switchPlayer() {
-  if (currentPlayer === player1) {
-    currentPlayer = player2;
-  } else {
-    currentPlayer = player1;
-  }
+	currentPlayer = currentPlayer ^ 1
 }
 
 // Function to handle a player's move
-function handleMove(row, col) {
-  // Check if the cell is already occupied
-  if (getCell(row,col) !== '') {
-    alert('Cell is already occupied!');
-    return;
-  }
+function handleMove(idx) {
+	// Check if the cell is already occupied
+	if (getCell(idx)) {
+		alert('Cell is already occupied!')
+		return
+	}
 
-  clicks++
+	clicks++
 
-  // Update the game board
-  setCell(row, col, currentPlayer);
+	// Update the game board
+	setCell(idx, players[currentPlayer])
 
-  // Switch to the other player's turn
-  switchPlayer();
+	// Switch to the other player's turn
+	switchPlayer()
 }
 
 // Function to reset the game
 function resetGame() {
-  // Clear the game board
-  for (let row = 0; row < 3; row++) {
-    for (let col = 0; col < 3; col++) {
-      setCell(row,col,'');
-    }
-  }
+	// Clear the game board
+	cells.forEach((celli, idx) => setCell(idx, ''))
 
-  // Reset the current player
-  currentPlayer = player1;
-  clicks = 0
+	// Reset the current player
+	currentPlayer = 0
+	clicks = 0
 }
 
 // Select the node that will be observed for mutations
 // Add event listeners to each cell on the game board
-const ttt = document.querySelector('.tic-tac-toe');
-const cells = ttt.querySelectorAll('td');
-cells.forEach((cell) => {
-  const [row, col] = cell.id.split('-').slice(1);
-  cell.addEventListener('click', () => {
-    handleMove(row, col);
-  });
-});
+cells.forEach((cell, idx) => {
+	cell.addEventListener('click', () => {
+		handleMove(idx)
+	})
+})
 
 
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
 	// Check for a winning combination
 	requestAnimationFrame(()=>setTimeout(() => {
-		const player = checkForWin()
-		if (player){
-			alert(`${player} wins!`);
-			resetGame();
+		for (const mutate of mutationList) {
+			const mark = checkForWin(mutate.target)
+			if (mark){
+				alert(`${mark} wins!`)
+				resetGame()
+				break
+			}
 		}
 		if (9 <= clicks){
 			alert('Draw!')
-			resetGame();
+			resetGame()
 		}
 	}, 0))
-};
+}
 
 // Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+const observer = new MutationObserver(callback)
 
 // Start observing the target node for configured mutations
 // Options for the observer (which mutations to observe)
